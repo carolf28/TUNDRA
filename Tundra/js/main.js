e2501ec149e81c71956d496e3c9f92d3c4a93ce5
Carolina Ribeiro <carolinaribeiro@Mac.Home>
2026-05-21 22:18:01 +0100

    spalsh screen

--- Tundra/js/main.js
+++ Tundra/js/main.js
@@ -1,13 +1,26 @@
-const label = document.getElementById("wheelLabel");
-const icons = document.querySelectorAll(".icon-wheel img");
+// wheel label functionality
 
-icons.forEach((icon) => {
+const icons = document.querySelectorAll(".icon-wheel img");
+const label = document.getElementById("wheelLabel");
+
+icons.forEach(icon => {
     icon.addEventListener("mouseenter", () => {
         label.textContent = icon.alt;
         label.classList.add("active");
     });
 
     icon.addEventListener("mouseleave", () => {
+        label.textContent = "";
         label.classList.remove("active");
     });
+});
+
+// splash screen functionality
+
+window.addEventListener("load", () => {
+    const splash = document.getElementById("splash-screen");
+
+    setTimeout(() => {
+        splash.classList.add("fade-out");
+    }, 2000); 
 });
